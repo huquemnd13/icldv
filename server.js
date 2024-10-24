a85c429dfd2ae453e1ed244309c36b2e013c4964
huquemnd13 <53827461+huquemnd13@users.noreply.github.com>
2024-10-24 09:57:05 -0600

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
